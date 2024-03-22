a4398e30f3a81d3433c92ed9dc6feedd6eb77516
Trey <trey.shearin3@gmail.com>
2024-03-21 21:35:15 -0400

    created const to hold connection information

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_tracker'
+});
