062a23c9ed3706583407fade43d0178a19d8c58d
Trey <trey.shearin3@gmail.com>
2024-03-21 21:47:57 -0400

    created function to initialize inquirer

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ connection.connect(err => {
     console.log('Connected to Employee Tracker.');
     //TODO Add function call to initialize app
 })
+
+function startTracker() {
+    inquirer.createPromptModule({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update an Employee Role',
+            'Quit',
+        ]
+    })
+}
