e216b993581b971338a7dd7d56ebe39b13234238
Trey <trey.shearin3@gmail.com>
2024-03-27 15:11:52 -0400

    created seeds for roles table

--- seeds.sql
+++ seeds.sql
@@ -3,3 +3,13 @@ INSERT INTO department (name) VALUES
 ('Finance'),
 ('Engineering'),
 ('Legal');
+
+INSERT INTO role (title, salary, department_id) VALUES
+('Lawyer', 190000, 4),
+('Legal Team Lead', 250000, 4),
+('Accountant', 125000, 2),
+('Account Manager', 160000, 2),
+('Software Engineer', 120000, 3),
+('Lead Engineer', 150000, 3),
+('Salesperson', 80000, 1),
+('Sales Lead', 100000, 1);
