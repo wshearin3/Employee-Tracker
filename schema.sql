81d11e11b1878c3669f8ed5ca7325da98e4a2d6d
Trey <trey.shearin3@gmail.com>
2024-03-27 14:10:28 -0400

    created table for department

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
